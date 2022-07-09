635f9b360b96578e571c538d9574133133a63892
diana2x <dianaclxcoe@gmail.com>
2022-07-08 22:48:34 -0500

    style and js added

---
+++ validation.js
@@ -0,0 +1,18 @@
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle"
+const cellElements = document.querySelectorAll('[data-cell]')
+let circleTurn
+
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+})
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn
+    // PlaceMark 
+    // check for Win 
+    // Check for Draw 
+    //  switch turns 
+}
